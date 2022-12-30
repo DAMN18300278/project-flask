8312ecc9abbc4e0745453928f7f0352ce8e33552
Pambipapu <d.i.e.g.o.alejandro@hotmail.com>
2022-12-29 20:58:10 -0600

    login

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from sys import flags
 from flask import Flask
 from flask import render_template
 from flask import Response
+from PIL import Image
 import mediapipe as mp
 import cv2
 
@@ -14,7 +14,7 @@ def generate():
     with mpFaceMesh.FaceMesh(
         static_image_mode = False,
         max_num_faces = 1,
-        min_detection_confidence = 0.2) as faceMesh:
+        min_detection_confidence = 0.1) as faceMesh:
         while True:
             ret, image = cap.read()
             if ret == False:
@@ -32,6 +32,23 @@ def generate():
                         mpFaceMesh.FACEMESH_CONTOURS, 
                         mpDrawing.DrawingSpec(color=(255, 0, 100), thickness=1, circle_radius=1),
                         mpDrawing.DrawingSpec(color=(255, 100, 0), thickness=1))
+
+            if image.shape[0] > image.shape[1]:
+                image = cv2.add(image, cv2.resize(marco, (image.shape[1], image.shape[0])))
+            else:
+                image = cv2.resize(image, (1300, 1000))
+                image = image[0:1000, 350:950]
+
+                marco = cv2.imread('contorno.png')
+                marco = cv2.resize(marco, (600, 600))
+
+                imageCut = image[200:800, 0:600]
+                imagenMezcla = cv2.addWeighted(imageCut, 1, marco, 1, 0)
+
+                image[200:800, 0:600] = imagenMezcla
+
+            #if(results.multi_face_landmarks[264])
+
             (flag, encodedImage) = cv2.imencode(".jpg", image)
             if not flag:
                 continue
@@ -39,7 +56,7 @@ def generate():
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("login.html")
 
 @app.route("/videoFeed")
 def videoFeed():
@@ -47,4 +64,4 @@ def videoFeed():
         mimetype="multipart/x-mixed-replace; boundary=frame")
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port="3000")
+    app.run(debug=True, host="0.0.0.0", port="3000")
